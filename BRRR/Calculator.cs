e22fc310c8dfc2b0618e725da8cd36107d1ed834
Zerefez <202209849@post.au.dk>
2025-01-27 19:38:49 +0100

    Intro

---
+++ BRRR/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        private static double Add(double a, double b)
+        {
+            return a + b;
+            
+        }
+        private static double Subtract(double a, double b)
+        {
+            return a - b;
+
+        }
+        private static double Multiply(double a, double b)
+        {
+            return a + b;
+
+        }
+        private static double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+    }
+}
