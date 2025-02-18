6cd297821c3312602bfc3d2bd2a33c787a4f669c
Zerefez <202209849@post.au.dk>
2025-02-18 17:33:42 +0100

    Update

--- Lection-00.1-01.2/Calculator/Calculator.cs
+++ Lection-00.1-01.2/Calculator/Calculator.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿﻿using System;
 
 namespace Calculator
 {
     public class CalculatorApp
     {
-
-         public double Accumulator {get; private set; }
+        // 01.2 OPGAVE
+        public double Accumulator {get; private set; }
 
         public double Add(double addend)
         {
@@ -55,9 +51,7 @@ namespace Calculator
             Accumulator = 0;
         }
 
-        /* 00.1 OPGAVE
-        public double Accumulator {get; private set; }
-
+        // 01.1 OPGAVE
         public double Add(double a, double b)
         {
             return a + b;
@@ -70,25 +64,13 @@ namespace Calculator
         }
         public double Multiply(double a, double b)
         {
-            return a + b;
+            return a * b;
 
         }
         public double Power(double x, double exp)
         {
             return Math.Pow(x, exp);
         }
-
-        public double Divide(double dividend, double divisor) {
-            if (divisor == 0) {
-                throw new DivideByZeroException();
-            }
-            return dividend / divisor;
-        }
-
-        public void Clear(){
-            Accumulator = 0;
-        }
-        */
 
     }
 }
